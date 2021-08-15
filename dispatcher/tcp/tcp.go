e87dae8e94eafd5264c113b1476d6470d3dd98f9
database64128 <free122448@hotmail.com>
2021-08-15 13:16:28 +0800

    🚠 Include client address in error messages of failed relay attempts

--- dispatcher/tcp/tcp.go
+++ dispatcher/tcp/tcp.go
@@ -87,7 +87,7 @@ func (d *TCP) handleConn(conn net.Conn) error {
 	defer pool.Put(buf)
 	n, err := io.ReadFull(conn, data)
 	if err != nil {
-		return fmt.Errorf("[tcp] handleConn readfull error: %w", err)
+		return fmt.Errorf("[tcp] %s <-x-> %s handleConn readfull error: %w", conn.RemoteAddr(), conn.LocalAddr(), err)
 	}
 
 	// get user's context (preference)
@@ -108,14 +108,14 @@ func (d *TCP) handleConn(conn net.Conn) error {
 	// dial and relay
 	rc, err := net.Dial("tcp", server.Target)
 	if err != nil {
-		return fmt.Errorf("[tcp] handleConn dial error: %w", err)
+		return fmt.Errorf("[tcp] %s <-> %s <-x-> %s handleConn dial error: %w", conn.RemoteAddr(), conn.LocalAddr(), rc.RemoteAddr(), err)
 	}
 	_ = rc.(*net.TCPConn).SetKeepAlive(true)
 
 	_ = rc.SetDeadline(time.Now().Add(DefaultTimeout))
 	_, err = rc.Write(data[:n])
 	if err != nil {
-		return fmt.Errorf("[tcp] handleConn write error: %w", err)
+		return fmt.Errorf("[tcp] %s <-> %s <-x-> %s handleConn write error: %w", conn.RemoteAddr(), conn.LocalAddr(), rc.RemoteAddr(), err)
 	}
 
 	log.Printf("[tcp] %s <-> %s <-> %s", conn.RemoteAddr(), conn.LocalAddr(), rc.RemoteAddr())
